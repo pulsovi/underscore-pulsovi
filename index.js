41981afad981f899b643273eaf11fbae8e650a49
David GABISON <david.gabison@outlook.com>
2020-11-24 16:12:09 +0100

    feat: added set function

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const underscore = require("underscore");
+const underscore = require('underscore');
 
-const { isUndefined } = underscore;
+const { isObject, isUndefined } = underscore;
 
 Object.assign(exports, underscore);
 
 function get(object, path) {
   let trail = null;
 
-  if ("" === path) trail = [];
-  else if ("string" === typeof path) trail = path.split(".");
+  if ('' === path) trail = [];
+  else if ('string' === typeof path) trail = path.split('.');
   else if (Array.isArray(path)) trail = path;
   else throw new TypeError(`path must be a String or an Array, ${typeof path} found.`);
 
@@ -22,6 +22,32 @@ exports.get = get;
 exports.int = val => val | 0;
 
 exports.isDefined = (...args) => !isUndefined(...args);
+
+function set(object, path, value) {
+  const trail = parseTrail(path);
+  const propertyName = trail.pop();
+  const owner = trail.reduce(
+    (reduced, next) => {
+      if (!isObject(reduced))
+        throw new TypeError(`Unable to set property ${next} on non Object value ${reduced}.`);
+      if (Object.keys(reduced).includes(next)) return reduced[next];
+      reduced[next] = {};
+      return reduced[next];
+    },
+    object
+  );
+
+  owner[propertyName] = value;
+
+  return object;
+}
+function parseTrail(path) {
+  if ('' === path) return [];
+  if ('string' === typeof path) return path.split('.');
+  if (Array.isArray(path)) return path.slice();
+  throw new TypeError(`path must be a String or an Array, ${typeof path} found.`);
+}
+exports.set = set;
 
 exports.sortBy = (array, key) => array.sort((item1, item2) => {
   if (get(item1, key) > get(item2, key)) return 1;
